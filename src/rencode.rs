2d48f0e2b2420f5289fef20658bf31298b8bdee8
The0x539 <the0x539@gmail.com>
2020-04-30 15:41:21 -0500

    wrapper methods for Write on RencodeSerializer

--- src/rencode.rs
+++ src/rencode.rs
@@ -73,9 +73,22 @@ type Result<T> = std::result::Result<T, Whatever>;
 
 struct RencodeSerializer(Vec<u8>, Vec<usize>);
 
+impl RencodeSerializer {
+    // This is a Vec, so if these writes fail, we have bigger problems.
+    fn write_all(&mut self, buf: &[u8]) { self.0.write_all(buf).unwrap(); }
+    fn write_u8(&mut self, n: u8) { self.0.write_u8(n).unwrap(); }
+    fn write_i8(&mut self, n: i8) { self.0.write_i8(n).unwrap(); }
+    fn write_i16(&mut self, n: i16) { self.0.write_i16::<BE>(n).unwrap(); }
+    fn write_i32(&mut self, n: i32) { self.0.write_i32::<BE>(n).unwrap(); }
+    fn write_i64(&mut self, n: i64) { self.0.write_i64::<BE>(n).unwrap(); }
+    fn write_f32(&mut self, n: f32) { self.0.write_f32::<BE>(n).unwrap(); }
+    fn write_f64(&mut self, n: f64) { self.0.write_f64::<BE>(n).unwrap(); }
+}
+
 pub fn to_bytes(value: &impl Serialize) -> Result<Vec<u8>> {
     let mut serializer = RencodeSerializer(vec![], vec![]);
     value.serialize(&mut serializer)?;
+    serializer.0.flush().unwrap();
     Ok(serializer.0)
 }
 
@@ -88,7 +101,7 @@ impl<'a> ser::SerializeSeq for &'a mut RencodeSerializer {
     }
     fn end(self) -> Result<()> {
         if self.1.pop().unwrap() >= LIST_COUNT {
-            self.0.write_u8(types::TERM).unwrap();
+            self.write_u8(types::TERM);
         }
         Ok(())
     }
@@ -102,7 +115,7 @@ impl<'a> ser::SerializeTuple for &'a mut RencodeSerializer {
     }
     fn end(self) -> Result<()> {
         if self.1.pop().unwrap() >= LIST_COUNT {
-            self.0.write_u8(types::TERM).unwrap();
+            self.write_u8(types::TERM);
         }
         Ok(())
     }
@@ -119,7 +132,7 @@ impl<'a> ser::SerializeMap for &'a mut RencodeSerializer {
     }
     fn end(self) -> Result<()> {
         if self.1.pop().unwrap() >= DICT_COUNT {
-            self.0.write_u8(types::TERM).unwrap();
+            self.write_u8(types::TERM);
         }
         Ok(())
     }
@@ -141,7 +154,7 @@ impl<'a> ser::Serializer for &'a mut RencodeSerializer {
     type SerializeStructVariant = Impossible;
 
     fn serialize_unit(self) -> Result<()> {
-        self.0.write_u8(types::NONE).unwrap();
+        self.write_u8(types::NONE);
         Ok(())
     }
 
@@ -154,64 +167,64 @@ impl<'a> ser::Serializer for &'a mut RencodeSerializer {
     }
 
     fn serialize_bool(self, v: bool) -> Result<()> {
-        self.0.write_u8(if v { types::TRUE } else { types::FALSE }).unwrap();
+        self.write_u8(if v { types::TRUE } else { types::FALSE });
         Ok(())
     }
 
     fn serialize_i8(self, v: i8) -> Result<()> {
         match v {
             0..=INT_POS_MAX => {
-                self.0.write_i8(INT_POS_START + v).unwrap();
+                self.write_i8(INT_POS_START + v);
             }
             INT_NEG_MIN..=-1 => {
-                self.0.write_i8(INT_NEG_START - 1 - v).unwrap();
+                self.write_i8(INT_NEG_START - 1 - v);
             }
             _ => {
-                self.0.write_u8(types::INT1).unwrap();
-                self.0.write_i8(v).unwrap();
+                self.write_u8(types::INT1);
+                self.write_i8(v);
             }
         }
         Ok(())
     }
 
     fn serialize_i16(self, v: i16) -> Result<()> {
-        self.0.write_u8(types::INT2).unwrap();
-        self.0.write_i16::<BE>(v).unwrap();
+        self.write_u8(types::INT2);
+        self.write_i16(v);
         Ok(())
     }
 
     fn serialize_i32(self, v: i32) -> Result<()> {
-        self.0.write_u8(types::INT4).unwrap();
-        self.0.write_i32::<BE>(v).unwrap();
+        self.write_u8(types::INT4);
+        self.write_i32(v);
         Ok(())
     }
 
     fn serialize_i64(self, v: i64) -> Result<()> {
-        self.0.write_u8(types::INT8).unwrap();
-        self.0.write_i64::<BE>(v).unwrap();
+        self.write_u8(types::INT8);
+        self.write_i64(v);
         Ok(())
     }
     
     fn serialize_f32(self, v: f32) -> Result<()> {
-        self.0.write_u8(types::FLOAT32).unwrap();
-        self.0.write_f32::<BE>(v).unwrap();
+        self.write_u8(types::FLOAT32);
+        self.write_f32(v);
         Ok(())
     }
 
     fn serialize_f64(self, v: f64) -> Result<()> {
-        self.0.write_u8(types::FLOAT64).unwrap();
-        self.0.write_f64::<BE>(v).unwrap();
+        self.write_u8(types::FLOAT64);
+        self.write_f64(v);
         Ok(())
     }
 
     fn serialize_bytes(self, v: &[u8]) -> Result<()> {
         let len = v.len();
         if len < STR_COUNT {
-            self.0.write_u8(STR_START + len as u8).unwrap();
+            self.write_u8(STR_START + len as u8);
         } else {
-            self.0.write(format!("{}:", len).as_bytes()).unwrap();
+            self.write_all(format!("{}:", len).as_bytes());
         }
-        self.0.write_all(v).unwrap();
+        self.write_all(v);
         Ok(())
     }
 
@@ -225,9 +238,9 @@ impl<'a> ser::Serializer for &'a mut RencodeSerializer {
 
     fn serialize_tuple(self, len: usize) -> Result<Self::SerializeTuple> {
         if len < LIST_COUNT {
-            self.0.write_u8(LIST_START + len as u8).unwrap();
+            self.write_u8(LIST_START + len as u8);
         } else {
-            self.0.write_u8(types::LIST).unwrap();
+            self.write_u8(types::LIST);
         }
         self.1.push(len);
         Ok(self)
@@ -236,9 +249,9 @@ impl<'a> ser::Serializer for &'a mut RencodeSerializer {
     fn serialize_map(self, len: Option<usize>) -> Result<Self::SerializeMap> {
         let len = len.ok_or(SerErr::custom("need to know map size ahead of time"))?;
         if len < DICT_COUNT {
-            self.0.write_u8(DICT_START + len as u8).unwrap();
+            self.write_u8(DICT_START + len as u8);
         } else {
-            self.0.write_u8(types::DICT).unwrap();
+            self.write_u8(types::DICT);
         }
         self.1.push(len);
         Ok(self)
