328695c6b5a7fe2998505bb61c2903676424e0be
The0x539 <the0x539@gmail.com>
2020-05-04 16:58:13 -0500

    initial rpc work

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
-use serde_json;
+//use serde::{Serialize, Deserialize};
+//use serde_json;
+
+use tokio::net::TcpStream;
+use tokio::prelude::*;
 
 mod rencode;
+mod rpc;
 
+/*
 fn main() {
-    let x = serde_json::json!([
-        8i64,
-        2,
-        ["abc", 0xdef, true],
-        {
-            "use_foo": false,
-            "amount": 3,
-        }
-    ]);
-    let encoded = rencode::to_bytes(&x).unwrap();
+    let y = rpc_request!(5, "foo", [1, 2, 3], {"foo" => "bar"});
+    let encoded = rencode::to_bytes(&y).unwrap();
     let decoded: serde_json::Value = rencode::from_bytes(&encoded).unwrap();
     println!("{}", serde_json::to_string_pretty(&decoded).unwrap());
+}
+*/
+
+#[tokio::main()]
+async fn main() {
+    let mut stream = TcpStream::connect("127.0.0.1:6142").await.unwrap();
+    println!("created stream");
+
+    let result = stream.write(b"hello world\n").await;
+    println!("wrote to stream; success={:?}", result.is_ok());
 }
---
+++ src/rpc.rs
@@ -0,0 +1,77 @@
+use std::collections::HashMap;
+use serde::{
+    Serialize, Serializer, ser::SerializeTuple,
+    //de::{Deserialize, Deserializer, Visitor, Error as _, Unexpected, EnumAccess, VariantAccess},
+};
+use serde_json::{self, Value};
+
+// NOTE: the server should receive a (serialized) list of these, not one
+pub struct RpcRequest {
+    pub request_id: i64,
+    pub method: &'static str,
+    pub args: Vec<Value>,
+    pub kwargs: HashMap<String, Value>,
+}
+
+impl Serialize for RpcRequest {
+    fn serialize<S: Serializer>(&self, ser: S) -> Result<S::Ok, S::Error> {
+        let mut s = ser.serialize_tuple(4)?;
+        s.serialize_element(&self.request_id)?;
+        s.serialize_element(&self.method)?;
+        s.serialize_element(&self.args)?;
+        s.serialize_element(&self.kwargs)?;
+        s.end()
+    }
+}
+
+#[macro_export]
+macro_rules! rpc_request {
+    (
+        $request_id:expr,
+        $method:expr,
+        [$($arg:expr),*],
+        {$($kw:expr => $kwarg:expr),+}
+        $(,)?
+    ) => {
+        {
+            use maplit::hashmap;
+            $crate::rpc::RpcRequest {
+                request_id: $request_id as i64,
+                method: $method,
+                args: vec![$(serde_json::json!($arg)),*],
+                kwargs: maplit::convert_args!(
+                    keys=String::from,
+                    values=serde_json::Value::from,
+                    hashmap!($($kw => $kwarg),*)
+                ),
+            }
+        }
+    };
+    ($request_id:expr, $method:expr, [$($arg:expr),*] $(,)?) => {
+        rpc_request!($request_id, $method, [$($arg),*], {})
+    };
+    ($request_id:expr, $method:expr, {$($kw:expr => $kwarg:expr),+} $(,)?) => {
+        rpc_request!($request_id, $method, [], {$($kw => $kwarg),*})
+    };
+    ($request_id:expr, $method:expr $(,)?) => {
+        rpc_request!($request_id, $method, [], {})
+    };
+}
+
+#[allow(dead_code)]
+pub enum RpcInbound {
+    Response {
+        request_id: i64,
+        return_value: Vec<Value>,
+    },
+    Error {
+        request_id: i64,
+        exception_type: String,
+        exception_msg: String,
+        traceback: String,
+    },
+    Event {
+        event_name: String,
+        data: Vec<Value>,
+    }
+}
