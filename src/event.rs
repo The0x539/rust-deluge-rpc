152fa904ad7cee7b6327e9013c7fddee0c6bd053
The0x539 <the0x539@gmail.com>
2020-05-24 14:04:53 -0500

    Event: move untuple sorcery to proc macro

--- src/event.rs
+++ src/event.rs
@@ -1,46 +1,9 @@
-use serde::{Serialize, Serializer, de, Deserialize};
+use serde::{Serialize, Serializer, Deserialize};
 use crate::types::{InfoHash, Value, List, TorrentState, IpAddr};
-use deluge_rpc_macro::rename_event_enum;
+use deluge_rpc_macro::rpc_events;
 use enum_kinds::EnumKind;
 
-struct UntupleVisitor<'de, T: Deserialize<'de>>(std::marker::PhantomData<(T, &'de ())>);
-impl<'de, T: Deserialize<'de>> de::Visitor<'de> for UntupleVisitor<'de, T> {
-    type Value = T;
-    fn expecting(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        f.write_str("a sequence containing a single value")
-    }
-    fn visit_seq<A: de::SeqAccess<'de>>(self, mut seq: A) -> Result<T, A::Error> {
-        seq.next_element()?.ok_or(de::Error::invalid_length(0, &"1"))
-    }
-}
-
-fn untuple<'de, D: de::Deserializer<'de>, T: Deserialize<'de>>(de: D) -> Result<T, D::Error> {
-    de.deserialize_tuple(1, UntupleVisitor(Default::default()))
-}
-
-struct UnitVisitor;
-impl<'de> de::Visitor<'de> for UnitVisitor {
-    type Value = ();
-    fn expecting(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        f.write_str("an empty list")
-    }
-    fn visit_seq<A: de::SeqAccess<'de>>(self, mut seq: A) -> Result<(), A::Error> {
-        match seq.size_hint() {
-            Some(0) => Ok(()),
-            Some(n) => Err(de::Error::invalid_length(n, &self)),
-            None => match seq.next_element::<Value>()? {
-                None => Ok(()),
-                Some(_) => Err(de::Error::invalid_value(de::Unexpected::Seq, &self)),
-            }
-        }
-    }
-}
-
-fn untuple0<'de, D: de::Deserializer<'de>>(de: D) -> Result<(), D::Error> {
-    de.deserialize_tuple(0, UnitVisitor)
-}
-
-#[rename_event_enum]
+#[rpc_events]
 #[enum_kind(EventKind, derive(Hash))]
 #[derive(Debug, Clone, Deserialize, EnumKind)]
 #[serde(tag = "0", content = "1")]
