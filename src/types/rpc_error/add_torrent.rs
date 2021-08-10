9a8c860b9f6a89d58d2b0b477f357e42393fcd05
The0x539 <the0x539@gmail.com>
2021-08-10 16:19:48 -0500

    use thiserror for error types

--- src/types/rpc_error/add_torrent.rs
+++ src/types/rpc_error/add_torrent.rs
@@ -1,58 +1,62 @@
 use crate::types::InfoHash;
 use hex::FromHex;
-use lazy_regex::regex;
 use lazy_static::lazy_static;
-use std::convert::From;
-use std::fmt;
+use regex::Regex;
+use std::str::FromStr;
+use thiserror::Error;
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Error)]
 pub enum Error {
+    #[error("Torrent already in session: {0}")]
     AlreadyInSession(InfoHash),
+    #[error("Torrent already being added: {0}")]
     AlreadyBeingAdded(InfoHash),
+    #[error("Invalid magnet info: {0}")]
     UnableToAddMagnet(String),
     // Shouldn't naturally occur for users of the API, but worth handling
+    #[error("Must specify a valid torrent")]
     MustSpecifyValidTorrent,
+    #[error("Decoding filedump failed: {0}")]
     DecodingFiledumpFailed(String),
+    #[error("Unable to add torrent to session: {0}")]
     UnableToAddToSession(String),
+    #[error("{0}")]
     Other(String),
 }
 
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Self::AlreadyInSession(hash) => write!(f, "Torrent already in session: {}", hash),
-            Self::AlreadyBeingAdded(hash) => write!(f, "Torrent already being added: {}", hash),
-            Self::UnableToAddMagnet(link) => write!(f, "Invalid magnet info: {}", link),
-            Self::MustSpecifyValidTorrent => f.write_str("Must specify a valid torrent"),
-            Self::DecodingFiledumpFailed(decode_ex) => {
-                write!(f, "Decoding filedump failed: {}", decode_ex)
-            }
-            Self::UnableToAddToSession(ex) => write!(f, "Unable to add torrent to session: {}", ex),
-            Self::Other(msg) => f.write_str(msg),
-        }
-    }
+lazy_static! {
+    static ref ALREADY_IN_SESSION: Regex =
+        Regex::new(r"^Torrent already in session \([0-9a-fA-F]{40}\)\.$").unwrap();
+    static ref ALREADY_BEING_ADDED: Regex =
+        Regex::new(r"^Torrent already being added \([0-9a-fA-F]{40}\)\.$").unwrap();
 }
 
-impl From<&str> for Error {
-    fn from(msg: &str) -> Self {
+static UNABLE_TO_ADD_MAGNET: &str = "Unable to add magnet, invalid magnet info: ";
+static MUST_SPECIFY_VALID_TORRENT: &str =
+    "You must specify a valid torrent_info, torrent state or magnet.";
+static UNABLE_TO_ADD_TORRENT: &str = "Unable to add torrent, decoding filedump failed: ";
+static UNABLE_TO_ADD_TO_SESSION: &str = "Unable to add torrent to session: ";
+
+impl FromStr for Error {
+    type Err = std::convert::Infallible;
+    fn from_str(msg: &str) -> Result<Self, Self::Err> {
         // TODO: order conditionals based on likelihood of occurrence
-        if regex!(r"^Torrent already in session \([0-9a-fA-F]{40}\)\.$").is_match(msg) {
-            Self::AlreadyInSession(InfoHash::from_hex(&msg[28..68]).unwrap())
-        } else if regex!(r"^Torrent already being added \([0-9a-fA-F]{40}\)\.$").is_match(msg) {
-            Self::AlreadyBeingAdded(InfoHash::from_hex(&msg[29..69]).unwrap())
-        } else if let Some(magnet) = msg.strip_prefix("Unable to add magnet, invalid magnet info: ")
-        {
-            Self::UnableToAddMagnet(magnet.to_string())
-        } else if msg == "You must specify a valid torrent_info, torrent state or magnet." {
+        Ok(if ALREADY_IN_SESSION.is_match(msg) {
+            let hash = InfoHash::from_hex(&msg[28..][..20]).unwrap();
+            Self::AlreadyInSession(hash)
+        } else if ALREADY_BEING_ADDED.is_match(msg) {
+            let hash = InfoHash::from_hex(&msg[29..][..20]).unwrap();
+            Self::AlreadyBeingAdded(hash)
+        } else if let Some(magnet) = msg.strip_prefix(UNABLE_TO_ADD_MAGNET) {
+            Self::UnableToAddMagnet(magnet.to_owned())
+        } else if msg == MUST_SPECIFY_VALID_TORRENT {
             Self::MustSpecifyValidTorrent
-        } else if let Some(decode_ex) =
-            msg.strip_prefix("Unable to add torrent, decoding filedump failed: ")
-        {
-            Self::DecodingFiledumpFailed(decode_ex.to_string())
-        } else if let Some(ex) = msg.strip_prefix("Unable to add torrent to session: ") {
-            Self::UnableToAddToSession(ex.to_string())
+        } else if let Some(decode_ex) = msg.strip_prefix(UNABLE_TO_ADD_TORRENT) {
+            Self::DecodingFiledumpFailed(decode_ex.to_owned())
+        } else if let Some(ex) = msg.strip_prefix(UNABLE_TO_ADD_TO_SESSION) {
+            Self::UnableToAddToSession(ex.to_owned())
         } else {
-            Self::Other(msg.to_string())
-        }
+            Self::Other(msg.to_owned())
+        })
     }
 }
--- src/types/rpc_error/mod.rs
+++ src/types/rpc_error/mod.rs
@@ -4,21 +4,16 @@ use add_torrent::Error as AddTorrentError;
 use crate::types::{Dict, List, Value};
 use serde::Deserialize;
 use std::convert::From;
-use std::fmt;
+use thiserror::Error;
 
-#[derive(Debug, PartialEq, Eq, Deserialize)]
+#[derive(Debug, PartialEq, Eq, Deserialize, Error)]
 #[serde(from = "(String, List, Dict, String)")]
+#[error("{traceback}")]
 pub struct GenericError {
     pub exception: String,
     pub args: List,
     pub kwargs: Dict,
     pub traceback: String,
-}
-
-impl fmt::Display for GenericError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        f.write_str(&self.traceback)
-    }
 }
 
 impl From<(String, List, Dict, String)> for GenericError {
@@ -32,28 +27,19 @@ impl From<(String, List, Dict, String)> for GenericError {
     }
 }
 
-#[derive(Debug, PartialEq, Eq, Deserialize)]
+#[derive(Debug, PartialEq, Eq, Deserialize, Error)]
 #[serde(from = "GenericError")]
 pub enum SpecializedError {
+    #[error("AddTorrentError: {0}")]
     AddTorrent(AddTorrentError),
+    #[error("{0}")]
     Generic(GenericError),
-}
-
-impl fmt::Display for SpecializedError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Self::AddTorrent(e) => write!(f, "AddTorrentError: {}", e),
-            Self::Generic(e) => f.write_str(&e.to_string()),
-        }
-    }
 }
 
 impl From<GenericError> for SpecializedError {
     fn from(err: GenericError) -> Self {
         match (err.exception.as_str(), err.args.as_slice()) {
-            ("AddTorrentError", [Value::String(msg)]) => {
-                Self::AddTorrent(AddTorrentError::from(msg.as_str()))
-            }
+            ("AddTorrentError", [Value::String(msg)]) => Self::AddTorrent(msg.parse().unwrap()),
             _ => Self::Generic(err),
         }
     }
